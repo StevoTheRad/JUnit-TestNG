1bb08c108d575fa609ea0e37991ec62bcc023f58
Sergey Petrenko <Stevo98@mail.ru>
2023-03-13 19:44:57 +1000

    Jupiter

---
+++ src/test/java/ru/netology/CashBackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashBackServiceTest {
+
+    @Test
+    public void ShouldReturnDifferenceIfBoundaryIsBigger() {
+        CashBackService service = new CashBackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturnZeroIfAmountEqualBoundary() {
+        CashBackService service = new CashBackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturnDifferenceIfAmountIsBigger() {
+        CashBackService service = new CashBackService();
+
+        int actual = service.remain(1600);
+        int expected = 400;
+
+        assertEquals(actual, expected);
+    }
+}
