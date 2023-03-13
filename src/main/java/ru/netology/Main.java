c0c7ecd2cfd752b570b7a67eb1c9f4fa4b276f5e
Sergey Petrenko <Stevo98@mail.ru>
2023-03-13 18:42:20 +1000

    JUnit4

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,6 +2,10 @@ package ru.netology;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 10;
+        int b = 10;
+        int c = a % b;
+        System.out.println(c);
     }
 }
+
