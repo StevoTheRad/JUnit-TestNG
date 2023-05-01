30d27d4ec0def309c12fa8d92dc74da926fd6c27
Sergey Petrenko <Stevo98@mail.ru>
2023-05-01 20:54:02 +1000

    new commit

---
+++ src/test/java/CashBackServiceTest.java
@@ -0,0 +1,44 @@
+import org.testng.annotations.Test;
+import ru.netology.CashBackService;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashBackServiceTest {
+
+
+    public class cashBackServiceTest {
+
+        @Test
+        public void ShouldReturnDifferenceIfBoundaryIsBigger() {
+            CashBackService service = new CashBackService();
+
+            int actual = service.remain(900);
+            int expected = 100;
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void ShouldReturnZeroIfAmountEqualBoundary() {
+            CashBackService service = new CashBackService();
+
+            int actual = service.remain(1000);
+            int expected = 0;
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void ShouldReturnDifferenceIfAmountIsBigger() {
+            CashBackService service = new CashBackService();
+
+            int actual = service.remain(1600);
+            int expected = 400;
+
+            assertEquals(actual, expected);
+        }
+    }
+
+
+
+}
