86d7036dd4f639e68ac36eb11a5464e2c44215eb
Sergey Petrenko <Stevo98@mail.ru>
2023-06-25 22:21:40 +1000

    commitnew

--- src/test/java/ru/netology/CashBackServiceJUnitJupiterTest.java
+++ src/test/java/ru/netology/CashBackServiceJUnitJupiterTest.java
@@ -1,11 +1,11 @@
 package ru.netology;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CashBackServiceTest {
+public class CashBackServiceJUnitJupiterTest {
 
     @Test
     public void ShouldReturnDifferenceIfBoundaryIsBigger() {
@@ -37,3 +37,4 @@ public class CashBackServiceTest {
         assertEquals(actual, expected);
     }
 }
+
